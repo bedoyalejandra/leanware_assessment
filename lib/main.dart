8d0b99e36294c53584f21ae46cae218e8b1e1c07
Alejandra Bedoya <52800189+bedoyalejandra@users.noreply.github.com>
2025-02-17 11:26:40 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:leanware_assessment/pages/LoginPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Leanware Technical Assessment',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
